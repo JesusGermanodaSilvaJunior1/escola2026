cdd5b00d6acc7aef09cf3483c032ba483e028b3a
JesusGermanodaSilvaJunior1 <jesusgermano@live.com>
2026-05-26 15:35:44 -0300

    (Criado a página index.php)

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta author="Jesus Germano da Silva Junior">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
